76417ca8e8c7e0bc37b8e9a7f27eedaf41cd6541
Jaydon <pearcejaydon@gmail.com>
2022-10-03 19:21:42 +1100

    Update config

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline text-red-500">Hello World</h1>
+      <h1 className="">Hello World</h1>
     </div>
   );
 }
