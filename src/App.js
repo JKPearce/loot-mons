93087ab47672d235a38b75133c2ba8e30bf5ddfb
Jaydon <pearcejaydon@gmail.com>
2022-10-03 19:16:51 +1100

    Install tailwind css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline text-red-500">Hello World</h1>
+    </div>
+  );
 }
 
 export default App;
