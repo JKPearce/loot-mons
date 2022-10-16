fd91f7838cdcd53b63ea051fde08164e4d18d13d
Jaydon <pearcejaydon@gmail.com>
2022-10-16 19:00:28 +1100

    Display inventory totals

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -9,7 +9,7 @@ const Inventory = () => {
       <div className="grid grid-cols-2 p-5 gap-5">
         <div className="card card-bordered bg-base-100 shadow-lg  col-span-full">
           <h3 className="z-10 sticky top-0 p-2 bg-base-300 text-center text-lg font-bold">
-            Pokemon
+            {`Pokemon Total:${pokemon.length}`}
           </h3>
           <div className="p-5 grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 gap-4 max-h-96 overflow-y-scroll">
             {pokemon.map((poke, i) => (
@@ -19,7 +19,7 @@ const Inventory = () => {
         </div>
         <div className="max-h-80 w-auto card card-bordered bg-base-100 shadow-lg">
           <h3 className="sticky top-0 p-2 bg-base-300 text-center text-lg font-bold">
-            Moves
+            {`Moves Total:${moves.length}`}
           </h3>
           <div className="overflow-y-scroll">
             <table className="table table-zebra w-full">
@@ -36,7 +36,7 @@ const Inventory = () => {
         </div>
         <div className="max-h-80 w-auto card card-bordered bg-base-100 shadow-lg">
           <h3 className="sticky top-0 p-2 bg-base-300 text-center text-lg font-bold">
-            Abilities
+            {`Abilities Total:${abilities.length}`}
           </h3>
           <div className="overflow-y-scroll">
             <table className="table table-zebra w-full">
