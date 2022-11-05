ac05df65b14b32241b7f7d37a684f6346bb6a1b7
Jaydon <pearcejaydon@gmail.com>
2022-11-05 14:45:43 +1100

    Half prices of boxes for testing

--- src/helpers/global.js
+++ src/helpers/global.js
@@ -1,11 +1,11 @@
 //credit cost of opening loot boxes
 export const PRICE = {
-  pokemon: 500,
-  abilities: 300,
-  moves: 100,
-  monBundle: 1000,
-  teamBundle: 5500,
-  firstTimeDiscount: 3000,
+  pokemon: 500 / 2,
+  abilities: 300 / 2,
+  moves: 100 / 2,
+  monBundle: 1000 / 2,
+  teamBundle: 5500 / 2,
+  firstTimeDiscount: 3000 / 2,
 };
 export const NEW_USER_CREDIT_AMOUNT = 7000;
 export const DAILY_CREDIT_AMOUNT = 1000;
